5bd386703f9edf23c28a73b49cdd2960e4944969
NikaNishnianidze <nikanishno8@gmail.com>
2025-03-01 17:09:37 +0400

    add checkbox and strength functions

--- script.js
+++ script.js
@@ -1,7 +1,10 @@
 const body = document.querySelector("body");
 const slider = document.getElementById("myRange");
 const sliderValue = document.getElementById("range-h3");
-var checkboxes = document.querySelectorAll("input[type = 'checkbox']");
+const checkbox1 = document.getElementById("myCheckbox1");
+const checkbox2 = document.getElementById("myCheckbox2");
+const checkbox3 = document.getElementById("myCheckbox3");
+const checkbox4 = document.getElementById("myCheckbox4");
 const strength = document.querySelector(".strength-box");
 const text = document.getElementById("strength-text");
 
@@ -11,6 +14,7 @@ slider.oninput = () => {
 };
 
 const passwordBox = document.getElementById("password");
+
 const upperCase = "QWERTYUIOPLKJHGFDSAZXCVBNM";
 const lowerCase = "qwertyuioplkjhgfdsazxcvbnm";
 const numbers = "1234567890";
@@ -20,10 +24,29 @@ const allChars = upperCase + lowerCase + numbers + symbols;
 
 function createPassword() {
   let password = "";
-  password += upperCase[Math.floor(Math.random() * upperCase.length)];
-  password += lowerCase[Math.floor(Math.random() * lowerCase.length)];
-  password += numbers[Math.floor(Math.random() * numbers.length)];
-  password += symbols[Math.floor(Math.random() * symbols.length)];
+
+  if (checkbox1.checked) {
+    password += upperCase[Math.floor(Math.random() * upperCase.length)];
+  } else if (checkbox2.checked) {
+    password += lowerCase[Math.floor(Math.random() * lowerCase.length)];
+  } else if (checkbox3.checked) {
+    password += numbers[Math.floor(Math.random() * numbers.length)];
+  } else if (checkbox4.checked) {
+    password += symbols[Math.floor(Math.random() * symbols.length)];
+  }
+
+  if (checkbox1.checked == true && checkbox2.checked == true) {
+    password += upperCase[Math.floor(Math.random() * upperCase.length)];
+    password += lowerCase[Math.floor(Math.random() * lowerCase.length)];
+  } else if (
+    checkbox1.checked == true &&
+    checkbox3.checked == true &&
+    checkbox2.checked == true
+  ) {
+    password += upperCase[Math.floor(Math.random() * upperCase.length)];
+    password += lowerCase[Math.floor(Math.random() * lowerCase.length)];
+    password += numbers[Math.floor(Math.random() * numbers.length)];
+  }
 
   while (length > password.length) {
     password += allChars[Math.floor(Math.random() * allChars)];
@@ -32,12 +55,80 @@ function createPassword() {
   passwordBox.value = password;
 }
 
+checkbox1.addEventListener("change", createPassword);
+checkbox2.addEventListener("change", createPassword);
+checkbox3.addEventListener("change", createPassword);
+checkbox4.addEventListener("change", createPassword);
+
 function check() {
-  checkboxes.forEach((checkbox) => {
-    if (checkbox.checked) {
-    }
-  });
+  let checkedCount = 0;
+
+  if (checkbox1.checked) {
+    body.classList.add("too-weak");
+    checkedCount++;
+    text.innerHTML = "TOO WEAK!";
+  } else {
+    body.classList.remove("too-weak");
+    text.innerHTML = "NONE";
+  }
+
+  if (checkbox2.checked) {
+    body.classList.add("weak");
+    checkedCount++;
+    text.innerHTML = "TOO WEAK!";
+  } else {
+    body.classList.remove("weak");
+    text.innerHTML = "NONE";
+  }
+
+  if (checkbox3.checked) {
+    body.classList.add("medium");
+    checkedCount++;
+    text.innerHTML = "TOO WEAK!";
+  } else {
+    text.innerHTML = "NONE";
+    body.classList.remove("medium");
+  }
+
+  if (checkbox4.checked) {
+    body.classList.add("strong");
+    checkedCount++;
+    text.innerHTML = "TOO WEAK!";
+  } else {
+    text.innerHTML = "NONE";
+    body.classList.remove("strong");
+  }
+
+  if (checkedCount == 2) {
+    body.classList.add("two-checked");
+    text.innerHTML = "WEAK";
+    body.classList.remove("three-checked", "four-checked", "one-checked");
+  } else if (checkedCount == 1) {
+    body.classList.add("one-checked");
+    text.innerHTML = "TOO WEAK!";
+    body.classList.remove("three-checked", "four-checked", "two-checked");
+  } else if (checkedCount == 3) {
+    body.classList.add("three-checked");
+    text.innerHTML = "MEDIUM";
+    body.classList.remove("two-checked", "four-checked", "one-checked");
+  } else if (checkedCount == 4) {
+    body.classList.add("four-checked");
+    text.innerHTML = "STRONG";
+    body.classList.remove("two-checked", "three-checked", "one-checked");
+  } else {
+    body.classList.remove(
+      "two-checked",
+      "one-checked",
+      "three-checked",
+      "four-checked"
+    );
+  }
 }
+
+checkbox1.addEventListener("change", check);
+checkbox2.addEventListener("change", check);
+checkbox3.addEventListener("change", check);
+checkbox4.addEventListener("change", check);
 
 function updateRangeBackground() {
   const value = slider.value;
